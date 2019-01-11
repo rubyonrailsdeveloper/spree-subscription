e1461d77908a48d253c383111f0d1c3b0dcf7a26
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2019-01-11 09:27:52 -0800

    changed migrate file

--- db/migrate/20120502150808_create_spree_subscriptions.rb
+++ db/migrate/20120502150808_create_spree_subscriptions.rb
@@ -1,4 +1,4 @@
-class CreateSpreeSubscriptions < ActiveRecord::Migration[5.2]
+class CreateSpreeSubscriptions < ActiveRecord::Migration[4.2]
   def change
     create_table :spree_subscriptions do |t|
       t.references :magazine
